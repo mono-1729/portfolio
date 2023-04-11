63b685fa502ace82ceda32959c4dac2ca7b1239a
mono-1729 <kawai.172957@gmail.com>
2023-04-11 22:25:15 +0900

    ファイル追加

---
+++ components/header.tsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h3>ポートフォリオ</h3>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">experiences</a>
+          </li>
+          <li>
+            <a href="">works</a>
+          </li>
+          <li>
+            <a href="">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="globals/material-ui/index.d.ts" />
+/// <reference path="modules/react-tap-event-plugin/index.d.ts" />
